8ad3f4c014300834654fb0149d61f45c90a53636
Jeff Mao <jeffmao@jeffs-mbp.attlocal.net>
2020-05-08 21:31:19 -0400

    Init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.static('public'));
+app.set('view engine', 'html');
+
+app.get('/', (req, res, next) => {
+    res.render('./index');
+});
+
+io.on('connection',(socket) => {
+    console.log('a user connected');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
